bbc9edb5c14aa3481324c4641f96e8b7f4134664
Andrew Wiliam <angularsprings403@gmail.com>
2026-03-30 04:04:00 +0700

    finish auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterStoreRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Exception;
+use Illuminate\Database\Eloquent\Attributes\UseResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            if (!Auth::guard('web')->attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'message' => 'Unauthorized',
+                    'data' => null
+                ], 401);
+            }
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json([
+                'message' => 'Login Berhasil',
+                'data' => [
+                    'token' => $token,
+                    'user' => new UserResource($user)
+                ]
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi Kesalahan',
+                'data' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function me()
+    {
+        try {
+            $user = Auth::user();
+
+              return response()->json([
+                'message' => 'Profile User Berhasil Diambil',
+                'data' => [
+                    'user' => new UserResource($user)
+                ]
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi Kesalahan',
+                'data' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function logout()
+    {
+        try {
+            $request = request();
+            $request->user()->currentAccessToken()->delete();
+            return response()->json([
+                'message' => 'Logout Berhasil',
+                'data' => null
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi Kesalahan',
+                'data' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+
+    public function register(RegisterStoreRequest $request)
+    {
+        $data = $request->validated();
+        
+        DB::beginTransaction();
+        try{
+            $user = new User;
+            $user->name = $data['name'];
+            $user->email = $data['email'];
+            $user->password = Hash::make($data['password']);
+            $user->save();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            DB::commit();
+            return response()->json([
+                'message' => 'Register Berhasil',
+                'data' => [
+                    'token' => $token,
+                    'user' => new UserResource($user)
+                ]
+            ], 201);
+            
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi Kesalahan',
+                'data' => $e->getMessage()
+            ], 500);
+        }
+    }   
+}
